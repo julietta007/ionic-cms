a575aedabf78bf272c88e3be859a0b0807274979
Julietta Hensgen <juliettaurdiales@yahoo.com>
2018-05-22 13:29:20 -0500

    Added users Page and service.

---
+++ src/pages/users/users.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UserProvider } from '../../providers/user/user';
+
+/**
+ * Generated class for the UsersPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html',
+})
+export class UsersPage {
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private userProvider: UserProvider
+  ) {
+  }
+
+  ionViewDidLoad() {
+    console.log(this.userProvider);
+  }
+
+}
